bee4f4a2b419354bae9bdc426f0ffc64f8f0714a
Administrator <administrator@nusco.corp>
2022-11-16 12:38:40 +0100

    a

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
 
 def articles(request):
     return render(request, 'blog/articles.html', {})
