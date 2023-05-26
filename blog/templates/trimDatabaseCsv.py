de6f51efb8ba51672399eb6d4a6b9aa08e6af89f
Luigi <gigitanzillo@gmail.com>
2023-05-26 12:12:16 +0200

    web app for export of Rilievi Nusco database

---
+++ blog/templates/trimDatabaseCsv.py
@@ -0,0 +1,224 @@
+# importing the pandas library
+import base64
+import os.path
+import zipfile
+from openpyxl import load_workbook
+
+import PySimpleGUI as Sg
+import pandas as pd
+
+layout = [
+    [Sg.Button('Converti'),
+     Sg.Input(key="-IN2-", readonly=True),
+     Sg.FileBrowse("Seleziona file", key="-IN-", file_types=(("File ZIP", ".zip"),)),
+     Sg.Button("Termina")],
+]
+
+window = Sg.Window('Window Title', layout, size=(600, 60), resizable=True)
+
+while True:
+
+    # try:
+        event, values = window.read()
+        print(values["-IN2-"])
+
+        # See if user wants to quit or window was closed
+        if event == Sg.WINDOW_CLOSED or event == "Termina":
+            window.close()
+            break
+        elif event == "Converti":
+            a = 0
+
+            zip_file = values["-IN-"]
+
+            zip_dir = os.path.dirname(zip_file)
+
+            zip_name = os.path.splitext(os.path.basename(zip_file))[0]
+
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                zip_ref.extractall(
+                    zip_dir + "/" + zip_name
+                )
+
+            unzippedFolder = zip_dir + "/" + zip_name
+
+            if not os.path.exists(unzippedFolder):
+                os.makedirs(unzippedFolder)
+
+            # reading the csv file
+            df = pd.read_csv(unzippedFolder + "/configurazionePersiane.csv", sep=";")
+            df1 = pd.read_csv(unzippedFolder + "/persiane.csv", sep=";")
+            df2 = pd.read_csv(unzippedFolder + "/tapparelle.csv", sep=";")
+
+            while a != len(df.index):
+                encodedImage = df.loc[a, "blob"]
+                if encodedImage == encodedImage:
+                    decodedImage = base64.b64decode(encodedImage)
+                    if not os.path.exists(unzippedFolder + "/Foto persiane"):
+                        os.makedirs(unzippedFolder + "/Foto persiane")
+                        # imageName = unzippedFolder + "/Foto persiane/" + df["riferimento"].values[a] + {} +
+                        # ".jpg".format(a)
+                        imageName = f"{unzippedFolder}/Foto persiane/{df['riferimento'].values[a]}{a}.jpg"
+                    else:
+                        # imageName = unzippedFolder + "/Foto persiane/" + df["riferimento"].values[a] + {} +
+                        # ".jpg".format(a)
+                        imageName = f"{unzippedFolder}/Foto persiane/{df['riferimento'].values[a]}{a}.jpg"
+                    # cambia il valore nelle celle della colonna blob con indice a
+
+                    df.loc[a, "blob"] = f"=COLLEG.IPERTESTUALE(\"{unzippedFolder}/Foto persiane/{df['riferimento'].values[a]}{a}.jpg\"; \"{df['riferimento'].values[a]}{a}.jpg\")"
+                    imgFile = open(imageName, "wb")
+                    imgFile.write(decodedImage)
+                    imgFile.close()
+                a = a + 1
+
+            a = 0
+
+            while a != len(df.index):
+                encodedDisegno = df.loc[a, "disegno"]
+                if encodedDisegno == encodedDisegno:
+                    decodedDisegno = base64.b64decode(encodedDisegno)
+                    if not os.path.exists(unzippedFolder + "/Disegno persiane"):
+                        os.makedirs(unzippedFolder + "/Disegno persiane")
+                        # disegnoName = unzippedFolder + "/Disegno persiane/" + df["riferimento"].values[a] + {} +
+                        # ".png".format(a)
+                        disegnoName = f"{unzippedFolder}/Disegno persiane/{df['riferimento'].values[a]}{a}.png"
+                    else:
+                        # disegnoName = unzippedFolder + "/Disegno persiane/" + df["riferimento"].values[a] + {} +
+                        # ".png".format(a)
+                        disegnoName = f"{unzippedFolder}/Disegno persiane/{df['riferimento'].values[a]}{a}.png"
+                    # cambia il valore nelle celle della colonna blob con indice a
+
+                    df.loc[a, "disegno"] = f"=COLLEG.IPERTESTUALE(\"{unzippedFolder}/Disegno persiane/{df['riferimento'].values[a]}{a}.png\"; \"{df['riferimento'].values[a]}{a}.png\")"
+                    imgFile = open(disegnoName, "wb")
+                    imgFile.write(decodedDisegno)
+                    imgFile.close()
+                a = a + 1
+
+            a = 0
+            # updating the column value/data
+            # df["blob"] = ""
+            # df["disegno"] = ""
+
+            # writing into the file
+            # df.to_csv(zip_dir + "/" + zip_name + "/Configurazione Persiane.csv", index=False, sep=";")
+            os.remove(unzippedFolder + "/configurazionePersiane.csv")
+
+            df_merged1 = df1.merge(df, how="inner", left_on=["id"], right_on="idParente")
+
+            df_merged1["id_y"] = ""
+            df_merged1["idParente"] = ""
+
+            # cancella le colonne
+            df_merged1.drop(["id_y", "idParente"], axis="columns", inplace=True)
+            df_merged1.drop(["larghezzaCassonetto"], axis="columns", inplace=True)
+            df_merged1.drop(["altezzaCassonetto"], axis="columns", inplace=True)
+            df_merged1.drop(["spessoreCassonetto"], axis="columns", inplace=True)
+            df_merged1.drop(["profonditaCielino"], axis="columns", inplace=True)
+
+            df_merged1.rename({"id_x": "id", "blob": "immagini"}, axis="columns", inplace=True)
+
+            os.remove(unzippedFolder + "/persiane.csv")
+
+            df = pd.read_csv(unzippedFolder + "/configurazioneTapparelle.csv", sep=";")
+
+            # persiane + configurazionePersiane
+            df_merged1.to_csv(zip_dir + "/" + zip_name + "/Persiane.csv", index=False, sep=";")
+
+            # TODO: ALLARGARE AUTOMATICAMENTE LE COLONNE
+            # df_merged1.to_excel(zip_dir + "/" + zip_name + "/Persiane.xlsx", index=False, header=True)
+            #
+            # writer = pd.ExcelWriter(zip_dir + "/" + zip_name + "/Persiane.xlsx", engine="openpyxl")
+            #
+            # df_excel = pd.read_excel(zip_dir + "/" + zip_name + "/Persiane.xlsx", engine="openpyxl")
+            #
+            # for column in df_excel:
+            #     column_length = max(df[column].astype(str).map(len).max(), len(column))
+            #     col_idx = df.columns.get_loc(column)
+            #     writer.sheets['Persiane'].set_column(col_idx, col_idx, column_length)
+            #
+            # writer.save()
+
+            print(df)
+
+            while a != len(df.index):
+                encodedImage = df.loc[a, "blob"]
+                if encodedImage == encodedImage:
+                    decodedImage = base64.b64decode(encodedImage)
+                    if not os.path.exists(unzippedFolder + "/Foto tapparelle"):
+                        os.makedirs(unzippedFolder + "/Foto tapparelle")
+                        # imageName = (unzippedFolder + "/Foto tapparelle/" + df["riferimento"].values[a] + {} +
+                        # ".jpg").format(a)
+                        imageName = f"{unzippedFolder}/Foto tapparelle/{df['riferimento'].values[a]}{a}.jpg"
+                    else:
+                        # imageName = (unzippedFolder + "/Foto tapparelle/" + df["riferimento"].values[a] + {} +
+                        # ".jpg").format(a)
+                        imageName = f"{unzippedFolder}/Foto tapparelle/{df['riferimento'].values[a]}{a}.jpg"
+                    # cambia il valore nelle celle della colonna blob con indice a
+
+                    df.loc[a, "blob"] = f"=COLLEG.IPERTESTUALE(\"{unzippedFolder}/Foto tapparelle/{df['riferimento'].values[a]}{a}.jpg\"; \"{df['riferimento'].values[a]}{a}.jpg\")"
+                    imgFile = open(imageName, "wb")
+                    imgFile.write(decodedImage)
+                    imgFile.close()
+                a = a + 1
+
+            a = 0
+
+            while a != len(df.index):
+                encodedDisegno = df.loc[a, "disegno"]
+                if encodedDisegno == encodedDisegno:
+                    decodedDisegno = base64.b64decode(encodedDisegno)
+                    if not os.path.exists(unzippedFolder + "/Disegno tapparelle"):
+                        os.makedirs(unzippedFolder + "/Disegno tapparelle")
+                        # disegnoName = unzippedFolder + "/Disegno tapparelle/" + df["riferimento"].values[a] + {} +
+                        # ".png".format(a)
+                        disegnoName = f"{unzippedFolder}/Disegno tapparelle/{df['riferimento'].values[a]}{a}.png"
+                    else:
+                        # disegnoName = unzippedFolder + "/Disegno tapparelle/" + df["riferimento"].values[a] + {} +
+                        # ".png".format(a)
+                        disegnoName = f"{unzippedFolder}/Disegno tapparelle/{df['riferimento'].values[a]}{a}.png"
+                    # cambia il valore nelle celle della colonna blob con indice a
+
+                    df.loc[a, "blob"] = f"=COLLEG.IPERTESTUALE(\"{unzippedFolder}/Disegno tapparelle/{df['riferimento'].values[a]}{a}.png\"; \"{df['riferimento'].values[a]}{a}.png\")"
+                    imgFile = open(disegnoName, "wb")
+                    imgFile.write(decodedDisegno)
+                    imgFile.close()
+                a = a + 1
+
+            a = 0
+            # updating the column value/data
+            # df["blob"] = ""
+            # df["disegno"] = ""
+
+            # writing into the file
+            # df.to_csv(zip_dir + "/" + zip_name + "/Configurazione Tapparelle.csv", index=False, sep=";")
+
+            df_merged2 = df2.merge(df, how="inner", left_on=["id"], right_on="idParente")
+
+            df_merged2["id_y"] = ""
+            df_merged2["idParente"] = ""
+
+            # cancella le colonne
+            df_merged2.drop(["id_y", "idParente"], axis="columns", inplace=True)
+
+            df_merged2.rename({"id_x": "id", "blob": "immagini"}, axis="columns", inplace=True)
+
+            os.remove(unzippedFolder + "/tapparelle.csv")
+            os.remove(unzippedFolder + "/configurazioneTapparelle.csv")
+
+            # tapparelle + configurazioneTapparelle
+            df_merged2.to_csv(zip_dir + "/" + zip_name + "/Tapparelle.csv", index=False, sep=";")
+
+            returned_value = Sg.popup_ok("Terminato", keep_on_top=True)
+            if returned_value == "OK":
+                os.startfile(unzippedFolder)
+
+            print(df)
+            print(values["-IN-"])
+
+        elif event == "Seleziona cartella":
+            print(values["-IN-"])
+    # except Exception as e:
+    #     Sg.popup_error_with_traceback(f'Errore.', e)
+    #     print(e)
+
+window.close()
